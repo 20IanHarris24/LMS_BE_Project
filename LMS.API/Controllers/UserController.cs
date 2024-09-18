b1043d49341dc63e30ca6bdeddb1f2bb7aa34f9b
Syldriem <93327634+Syldriem@users.noreply.github.com>
2024-09-18 13:52:49 +0200

    getusers

--- LMS.API/Controllers/UserController.cs
+++ LMS.API/Controllers/UserController.cs
@@ -1,10 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LMS.API.Data;
+using LMS.API.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace LMS.API.Controllers
 {
-    [Route("api/authentication")]
+    [Route("api/users")]
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly DatabaseContext _context;
+        public UserController(DatabaseContext context)
+        {
+            this._context = context;
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ApplicationUser>>> GetUsers() 
+        {
+            var users = await _context.Set<ApplicationUser>().ToListAsync();
+            return Ok(users);
+        }
+        public async Task<ActionResult<ApplicationUser>> GetUser(Guid id) 
+        {
+            var users = await _context.Set<ApplicationUser>().FirstOrDefaultAsync(user => user.Id == id);
+            return Ok(users);
+        }
     }
 }
