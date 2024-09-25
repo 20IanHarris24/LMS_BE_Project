3b5058a94136635639331778d404faa143768b93
Panas425 <anasabualsoud@hotmail.com>
2024-09-25 15:35:58 +0200

    added refresh token controller

---
+++ LMS.API/Controllers/TokenController.cs
@@ -0,0 +1,30 @@
+﻿using LMS.API.Models.Dtos;
+using LMS.API.Service.Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMS.API.Controllers
+{
+    [Route("api/token")]
+
+    [ApiController]
+
+    public class TokenController(IAuthService authenticationService) : ControllerBase
+
+    {
+
+        [HttpPost("refresh")]
+
+        public async Task<ActionResult<TokenDto>> RefreshToken(TokenDto token)
+
+        {
+
+            TokenDto tokenDto = await authenticationService.RefreshTokenAsync(token);
+
+            return Ok(tokenDto);
+
+        }
+
+    }
+
+}
