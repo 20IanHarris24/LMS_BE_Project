1f7973655de9278f697ba89a9505d079fcb18d45
milanesiluca <luca.milanesi@live.se>
2024-10-03 12:06:14 +0200

    removed wrong end parameter from entity and DTO

--- LMS.API/Models/Entities/Course.cs
+++ LMS.API/Models/Entities/Course.cs
@@ -9,8 +9,8 @@ namespace LMS.API.Models.Entities
         public string Description { get; set; }
         [Required]
         public DateTime Start { get; set; }
-        [Required]
-        public DateTime End { get; set; }
+        //[Required]
+        //public DateTime End { get; set; }
 
         public IEnumerable<Module> Modules { get; set; }
         public IEnumerable<ApplicationUser>? Users { get; set; }
